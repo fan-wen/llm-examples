10a21716de4bb0fa14eaa7cc497b30bcedb655df
Fan Wen <github@wen.fan>
2023-11-24 21:08:11 -1000

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -34,6 +34,7 @@ if prompt := st.chat_input():
     run = st.session_state.client.beta.threads.runs.create(
         thread_id=st.session_state.thread.id,
         assistant_id = "asst_HoFab7RBf3Df2PgqMi7CWL9S",
+        tools = ["retrieval"]
         instructions = "Please address the user as Fan"
     )
     while True:
