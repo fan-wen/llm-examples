dcefdbb277ce1eea2ff2feed36f67384ca8d332d
Fan Wen <github@wen.fan>
2023-11-24 20:32:41 -1000

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -15,7 +15,7 @@ if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    st.chat_message(msg["role"]).write(msg["content"]["text"]["value"])
 
 if prompt := st.chat_input():
     if not openai_api_key:
