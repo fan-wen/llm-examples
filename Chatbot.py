94dcd304abad7c7f20bca439f3a4b20747f44ab6
Fan Wen <github@wen.fan>
2023-11-24 20:14:24 -1000

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -19,8 +19,8 @@ for msg in st.session_state.messages:
 
 if prompt := st.chat_input():
     if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.")
-    st.stop()
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
     client = OpenAI(api_key=openai_api_key)
     st.session_state.thread = st.session_state.client.beta.threads.create()
     message = st.session_state.client.beta.threads.messages.append(
